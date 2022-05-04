bd65da7804145eb52bdc5322e180addff4d6a77e
SonTonyD <79934633+SonTonyD@users.noreply.github.com>
2022-05-04 11:39:37 +0200

    Edit init.sql Varchar(150) pour url

--- init.sql
+++ init.sql
@@ -6,11 +6,11 @@ USE PManager;
 
 CREATE TABLE users (
     IdUser INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(30) NOT NULL DEFAULT 'toto',
+    name VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
     token TEXT,
     id CHAR(50),
     email VARCHAR(255),
-    url VARCHAR(30),
+    url VARCHAR(150),
     PRIMARY KEY (IdUser) USING BTREE
 )
 COLLATE='utf8mb3_general_ci'
@@ -65,4 +65,5 @@ INSERT INTO files (Name, DateLastModif, IdOwner) VALUES ('testC1', '2022-03-15 1
 INSERT INTO files (Name, DateLastModif, IdOwner) VALUES ('testC2', '2022-03-15 18:42:42',2);
 INSERT INTO files (Name, DateLastModif, IdOwner) VALUES ('testC3', '2022-03-15 18:42:42',2);
 
-UPDATE files SET File="fichier test" WHERE IdFile=1;
+UPDATE files SET File="fichier test" WHERE IdFile=1
+
